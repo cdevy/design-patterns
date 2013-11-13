db3e5c1602b6b8e4fd0038893bed7a23f0e8bb43
Francois Charoy <charoy@gmail.com>
2013-11-13 16:16:01 +0100

    remove unused import

--- src/eu/telecomnancy/Client.java
+++ src/eu/telecomnancy/Client.java
@@ -12,8 +12,6 @@ package eu.telecomnancy;
  */
 
 import java.util.Scanner;
-import java.rmi.RemoteException;
-
 
 /**
  *
@@ -26,7 +24,7 @@ public class Client {
         capteur=c;
     }
 
-    public void menu() throws RemoteException {
+    public void menu()  {
         char rep='x';
         while (rep != 'q') {
             try {
@@ -57,23 +55,4 @@ public class Client {
         }
     }
 
-//   Code pour JDK 1.6
-//
-//       public void menu6() {
-//        String rep="";
-//        while (!"q".equals(rep)) {
-//            try {
-//                rep= c.nextLine();
-//                if ("o".equals(rep))
-//                        sense.onOff();
-//                else if ("s".equals(rep))
-//                        System.out.println("status :"+sense.getStatus());
-//                else if ("v".equals(rep))
-//                        System.out.println("value :"+sense.getValue());
-//                else System.out.println("q: quitter - o: switch - s: status - v: value");
-//            } catch (SensorNotActivated ex) {
-//                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-//    }
 }
